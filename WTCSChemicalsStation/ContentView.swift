6df3337a135941b99a5cc711b483562072f5e05d
Nestorov13 <nes7orov@gmail.com>
2022-11-19 11:23:33 +0200

    Initial Commit

---
+++ WTCSChemicalsStation/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WTCSChemicalsStation
+//
+//  Created by Borislav on 18.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+   
+       Text("Hello!")
+  
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WTCSChemicalsStation/WTCSChemicalsStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTCSChemicalsStationApp.swift
+//  WTCSChemicalsStation
+//
+//  Created by Borislav on 18.09.22.
+//
+
+import SwiftUI
+
+@main
+struct WTCSChemicalsStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
